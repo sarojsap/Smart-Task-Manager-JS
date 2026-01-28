e006313c8e3f5ecc10dd8bb13f825d22b5c30ebd
Saroj <sarojsap985@gmail.com>
2026-01-28 21:31:13 +0545

    feat: add debounce utility for search optimization

--- src/utils.js
+++ src/utils.js
@@ -17,3 +17,11 @@ export const formatDate = (dateString) => {
 
   return date.toLocaleDateString();
 };
+
+export const debounce = (fn, delay) => {
+    let timeoutId;
+    return (...args) => {
+        if (timeoutId) clearTimeout(timeoutId);
+        timeoutId = setTimeout(() => fn(...args), delay);
+    };
+};
